e0586265287868637b4104bcfd8f370310abdbd9
gillian <gamchihuahua@gmail.com>
2026-07-31 10:50:27 -0400

    Day 13 and 14 code

---
+++ 13-day/main.py
@@ -0,0 +1,75 @@
+# Day 13
+# Reviewing debugging programs
+
+# Example 1 - if number % 2 needs to be '==' to 0
+'''
+def odd_or_even(number):
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+odd_or_even(5)
+'''
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+print(odd_or_even(5))
+
+# Example 2 - year % 4000 is wrong, should be 400
+'''
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 4000 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+'''
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+print(is_leap(2000))
+
+# Example 3 - if statements should be elif statements
+# the for if statement should be 'and' and not 'or'
+# Final print statement should be print(number)
+'''
+Target is the number up to which we count
+def fizz_buzz(target):
+   for number in range(1, target + 1):
+       if number % 3 == 0 or number % 5 == 0:
+           print("FizzBuzz")
+       if number % 3 == 0:
+           print("Fizz")
+       if number % 5 == 0:
+           print("Buzz")
+       else:
+           print([number])
+'''
+#Target is the number up to which we count
+def fizz_buzz(target):
+   for number in range(1, target + 1):
+       if number % 3 == 0 and number % 5 == 0:
+           print("FizzBuzz")
+       elif number % 3 == 0:
+           print("Fizz")
+       elif number % 5 == 0:
+           print("Buzz")
+       else:
+           print(number)
+
+fizz_buzz(15)
---
+++ 14-day/main.py
@@ -0,0 +1,51 @@
+#Day 14 - Higher or Lower Game
+# Using all python from the class so far in one game
+
+import os
+import random
+from game_data import data
+import art
+
+def check_guess(user_guess, option_a, option_b):
+    if user_guess == "A":
+        if option_a["follower_count"] >= option_b["follower_count"]:
+            return True
+        else:
+            return False
+    elif user_guess == "B":
+        if option_a["follower_count"] <= option_b["follower_count"]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def print_vs(option_a, option_b):
+    print(f"Compare A: {option_a["name"]}, a {option_a["description"]}, from {option_a["country"]}")
+    print(art.vs)
+    print(f"Compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}")
+
+def play_game(option_a, option_b, score):
+    guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+    if check_guess(guess, option_a, option_b):
+        score += 1
+        option_a = option_b
+        option_b = random.choice(data)
+        os.system("clear")
+        print(art.logo)
+        print(f"You're right! Current score: {score!s}.")
+        print_vs(option_a, option_b)
+        play_game(option_a, option_b, score)
+    else:
+        os.system("clear")
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score!s}")
+
+option_a = random.choice(data)
+option_b = random.choice(data)
+score = 0
+
+os.system("clear")
+print(art.logo)
+print_vs(option_a, option_b)
+play_game(option_a, option_b, score)
