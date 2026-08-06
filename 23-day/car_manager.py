5b86850f91907494d99052db411b3dd764a9c005
gillian <gamchihuahua@gmail.com>
2026-08-06 14:45:01 -0400

    Day 23, Turtle Crossing

---
+++ 23-day/car_manager.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.cars = []
+        self.move_speed = STARTING_MOVE_DISTANCE
+
+    def generate_car(self):
+        t = Turtle()
+        t.shape("square")
+        t.shapesize(1,2)
+        t.penup()
+        t.setheading(180)
+        car_color = random.choice(COLORS)
+        t.color(car_color)
+        y_axis = random.randint(-250, 250)
+        t.goto(300, y_axis)
+        self.cars.append(t)
+
+    def generate_cars(self):
+        car_gen = random.randint(1,6)
+        if car_gen == 1:
+            self.generate_car()
+
+    def move(self):
+        for car in self.cars:
+            car.forward(self.move_speed)
+
+    def increase_speed(self):
+        self.move_speed += MOVE_INCREMENT
+
+    def car_collision(self,player_pos):
+        for car in self.cars:
+            if car.distance(player_pos) < 20:
+                return True
---
+++ 23-day/main.py
@@ -0,0 +1,36 @@
+# Day 23 - Turtle Crossing
+# A turtle's take on the classic frogger game
+
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car.generate_cars()
+    car.move()
+    if car.car_collision(player.position()):
+        scoreboard.game_over()
+        game_is_on = False
+
+    if player.next_level():
+        player.start_position()
+        car.increase_speed()
+        scoreboard.level_up()
+
+screen.exitonclick()
