e68c7bdbd911ff285b6eb8376d9f39e6bf036084
gillian <gamchihuahua@gmail.com>
2026-08-10 23:41:03 -0400

    Add highest score tracking day 20 snake

--- 20-day/scoreboard.py
+++ 20-day/scoreboard.py
@@ -11,15 +11,19 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.h_score = 0
         self.hideturtle()
         self.penup()
         self.color("white")
         self.goto(0, 260)
+        self.get_high_score()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(
-            f"Score: {self.score}", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_TYPE)
+            f"Score: {self.score}     High Score: {self.h_score}",
+            align=ALIGNMENT,
+            font=(FONT, FONT_SIZE, FONT_TYPE),
         )
 
     def update_score(self):
@@ -28,5 +32,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def game_over(self):
+        self.update_high_score()
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_TYPE))
+
+    def get_high_score(self):
+        try:
+            with open("high-score.txt", "r") as f:
+                self.h_score = int(f.readline())
+        except FileNotFoundError:
+            pass
+
+    def update_high_score(self):
+        if self.h_score <= self.score:
+            with open("high-score.txt", "w") as f:
+                f.write(f"{self.score}")
